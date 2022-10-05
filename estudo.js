ad9238a58a443264159dc708f975813c82d8bbf2
biarafaela10 <110104606+biarafaela10@users.noreply.github.com>
2022-10-05 09:57:46 -0300

    05/10/2022

--- estudo.js
+++ estudo.js
@@ -1,1 +1,35 @@
+let tabuada = 2;
+let nome = "beatriz rafaela";
 
+function escreva(){
+
+    document.write(tabuada +" x 1 = "+(tabuada*1)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*2)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*3)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*4)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*5)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*6)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*7)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*8)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*9)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*10)+"<br>");
+    
+}
+//for(inicio;validaçao: incremento)
+function minhaTabuada(){
+    for(let 1 = 5; i <=10; i++){
+    document.write((tabuada +" x " +i+"="+(tabuada*8)+"<br>"); 
+    for(let 1= 1; i <=10; j++)
+    document.write( i +" x "+j+"=" + "+(j+i)+"<br>");
+
+        document.write("<br>");
+    }
+
+    }
+    function cubo(){
+     for(let i = 1; i <= 20; i++){
+        document.write("O cubo  de " + i + " é " +i+ "x"+i+<br>"); 
+
+     }
+
+  
