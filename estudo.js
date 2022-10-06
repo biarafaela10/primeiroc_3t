287ff0ad6be53046e0f7ec63600b22144b3ce236
biarafaela10 <110104606+biarafaela10@users.noreply.github.com>
2022-10-06 12:46:26 -0300

    06/10/2022

--- estudo.js
+++ estudo.js
@@ -28,8 +28,22 @@ function minhaTabuada(){
     }
     function cubo(){
      for(let i = 1; i <= 20; i++){
-        document.write("O cubo  de " + i + " é " +i+ "x"+i+<br>"); 
+        document.write("O cubo  de " + i + " é " + i + " x " + i + "<br>"); 
 
      }
+  
+     function mat(){
+        let V = document.getElementById("valor").value;
+        let j = document.getElementById("juros").value;
+        let t = document.getElementById("meses").value;
+        let r=0;
+        for(let i=1; i<=t; i++){
+             r= v+(1+)(j/100);
+             v=r;
 
+        }
+         document.write("Resultado "+ r);
+
+
+     }
   
